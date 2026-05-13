d299c828a76778a8d7b8f7c6ba1ed6c2e8fe3a9c
Marc-Andre <mwessner@student.ethz.ch>
2026-05-13 16:08:23 +0200

    chenge tooling to peekrdl

--- rtl/adc_acquisition/reg/generate.sh
+++ rtl/adc_acquisition/reg/generate.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-# NOTE THIS IS RUN INSIDE THE OPENTITAN DOCKER CONTAINER
+# Note we use peakrdl for generation
+echo "Generating ADC Acq Registers & Docs"
 
-# Use this bash script to generate
-# docs, register, c ....
-# from the register configuration
-# note: it uses regtool from opentitan
-
-cd /home/dev
-echo "GENERATING ADC REGISTERS"
-
-REGTOOL=opentitan/util/regtool.py
-
-REGDEFINITION=src/rtl/adc_acquisition/reg/adc_acquisition_reg_definition.hjson
-
-# Generate the RTL
-"$REGTOOL" src/rtl/adc_acquisition/reg/adc_acquisition_reg_definition.hjson -r --outdir src/rtl/adc_acquisition/reg
-# Generate the Markdown
+peakrdl regblock -o . adc_acquisition_reg_definition.rdl --cpuif obi-flat
